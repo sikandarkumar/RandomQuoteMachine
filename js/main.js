26513be420fe6a60250e319f01183b72223fb6de
Sikandar Kumar <sikandar@example.com>
2017-03-02 20:39:10 +0530

    Fixed the twitter button

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@ function randomQuote() {
       success: function( response ) {
         $("#quoteDisplay").html("<p id='random_quote' class='lead'>" +
           response.quoteText + "<br/>&dash; " + response.quoteAuthor + "</p>");
-        $("#tweet").attr("href", "http://twitter.com/home/?status=" + response.quoteText +
-          ' (' + response.quoteAuthor + ')');
+
+           text=response.quoteText;
+           auth=response.quoteAuthor;
+        // $('#tweet').attr("href", "http://twitter.com/home/?status=" + response.quoteText +
+        //     ' (' + response.quoteAuthor + ')');
       }
   });
 }
@@ -16,6 +19,14 @@ $(function() {
   randomQuote();
 });
 
-$("button").click(function(){
+$("#get-another-quote-button").click(function(){
   randomQuote();
 });
+
+$("#tweet").on("click", function() {
+    window.open("http://twitter.com/home/?status=" +text+' (' +auth+ ')', '_blank');
+    })
+
+//Changing the background randomly will go here
+
+var backgroundColors=["#2C3539","#25383C","#98AFC7","#151B54","#1F45FC","#254117","#493D26","#E55451"];
